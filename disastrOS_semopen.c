d1ca7939a06354b67f67efad070ef07ec190d279
Pietro Manganelli Conforti <pietro.Manganelliconforti@gmail.com>
2019-07-28 23:48:02 +0200

    migliorie semOpen

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -9,17 +9,29 @@
 void internal_semOpen(){
 
 	int id = running->syscall_args[0]; //id 
-	if(id<0)
+
+	if(id<0){
+		printf("errore id invalido");
 		return;
+	}
 
 	Semaphore* sem=SemaphoreList_byId(&semaphore_lst,id); //vedo se esiste un sem associato
+
 	if (!sem){
-        sem = Semaphore_alloc(id, 1); //in caso lo creo (type=1) e aggiungo in coda
+       		sem = Semaphore_alloc(id, 1); //in caso lo creo (type=1) e aggiungo in coda
+			if(!sem) {
+				printf("errore nell'allocazione del semaforo!");
+				return;
+			}
         List_insert(&semaphores_list, semaphores_list.last, (ListItem*) sem);
     	}
 
 	SemDescriptor* sd= SemDescriptor_alloc(running->last_fd,sem,running);
-	if(!sd) return;
+
+	if(!sd) {
+		printf("errore nell'allocazione del semDescriptor!");
+		return;
+	}
 
 	running->last_fd++; //incremento fd
 
@@ -28,21 +40,10 @@ void internal_semOpen(){
 	
 	//lo aggiungo 
 	sd->ptr=sd_ptr;
-	List_insert(&running->sem_descriptors,sem->descriptors.last,(ListItem*) sd_ptr);
+	List_insert(&sem->descriptors,sem->descriptors.last,(ListItem*) sd_ptr);
 	
 	running->syscall_retvalue = sd->fd;
 
 	return;
-
-
-	
-  // do stuff :)
-  // creates a semaphore in the system, having num semnum
-  // the semaphore is accessible throughuot the entire system
-  // by its id.
-  // on success, the function call returns semnum (>=0);
-  // in failure the function returns an error code <0
-  //- int s=DisastrOS_semOpen(int semnum)
-
 
 }
