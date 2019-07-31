f7ad5a458f066bc4ae0da1bf56f40f89c94fab94
Pietro Manganelli Conforti <pietro.Manganelliconforti@gmail.com>
2019-07-31 15:16:24 +0200

    fix semClose

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -20,7 +20,7 @@ void internal_semClose(){
 	SemDescriptor* sd= SemDescriptorList_byFd(&running->sem_descriptors, id);
 
  	if(!sd){
-  		 printf("[ERSEMC] Errore nella semClose del semaforo con id:%d \n",id);
+  		 printf("[ERSEMC] Errore nella semClose del semaforo\n");
 		 // running->syscall_retvalue=DSOS_ERESOURCECLOSE;
    		 return;
   	}
