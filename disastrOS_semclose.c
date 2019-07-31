976f901fba1fa1c59101c935fce3ac5819298125
Pietro Manganelli Conforti <pietro.Manganelliconforti@gmail.com>
2019-07-31 15:15:22 +0200

    fix semClose

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -10,28 +10,31 @@ void internal_semClose(){
 
 	//seguendo la disastrOS_close_resource
 
-	int id = running->syscall_args[0]; //1 prendo l'id 
+	int id = running->syscall_args[0]; //1 prendo l'id del semaforo 
 
 	if(id<0){
-		printf("[SEM] errore id invalido %d \n",id);
+		printf("[ERSEMC] Errore id invalido %d \n",id);
 		return;
 	}
 
 	SemDescriptor* sd= SemDescriptorList_byFd(&running->sem_descriptors, id);
 
  	if(!sd){
-  		 printf("[SEM] errore nella semClose \n");
+  		 printf("[ERSEMC] Errore nella semClose del semaforo con id:%d \n",id);
 		 // running->syscall_retvalue=DSOS_ERESOURCECLOSE;
    		 return;
   	}
 
- 	//3 rimuovo il descriptor dalla lista dei processi
+ 	//3 rimuovo il descriptor dalla lista dei sem_desc del pcb
  	sd = (SemDescriptor*)List_detach(&running->sem_descriptors, (ListItem*)sd);
   	assert(sd);
 
  	// prendo la risorsa(semaforo)
   	Semaphore* sem = sd->semaphore;
-
+	if(sem==0){
+		printf("[ERSEMC] Errore nella chiusura del semaforo\n");
+		return;	
+	}
  	//4 rimuovo il puntatore al descriptor dalla resource list
  	SemDescriptorPtr* sd_ptr = (SemDescriptorPtr*)List_detach(&sem->descriptors, (ListItem*)(sd->ptr));
   	assert(sd_ptr);
@@ -39,9 +42,15 @@ void internal_semClose(){
   	SemDescriptor_free(sd);
   	SemDescriptorPtr_free(sd_ptr);
   
-  	running->syscall_retvalue = 0;
 
-	printf("[SEM] Semaforo con id:%d chiuso. \n",id);
+	//rimuovo il semaforo dalla lista
+	if(sem->descriptors.size==0 && sem->waiting_descriptors.size==0){
+		List_detach(&semaphores_list,(ListItem*)sem);
+		Semaphore_free(sem);
+		printf("[SEM] Semaforo con id:%d chiuso. \n",id);
+		}
+
+	running->syscall_retvalue = 0;
 
 	return;
 }
